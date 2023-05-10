3a5339f16b66f5df245ddcb045f504ab6fe4bfb5
KalinchenkoDA <130779365+KalinchenkoDA@users.noreply.github.com>
2023-05-10 21:16:19 +0300

    Add files via upload

---
+++ KalinchenkoDA.cpp
@@ -0,0 +1,113 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(0));
+    
+    // Задание 1.
+    /*cout << "Введите четыре числа" << endl;
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    double sa = (a + b + c + d) / 4;
+    cout << "Среднее арифметическое равно: " << sa << endl;*/
+
+    // Задание 2.
+    /*cout << endl << "Четные числа в диапазоне от 1 до 40: " << endl;
+    for (int i = 0; i < 40; ) {
+        i = i + 2;
+        cout << i << "  ";
+    }*/
+
+    // Задание 3.
+    /*int kvadrat;
+    cout << endl << "Введите размер стороны квадрата: " << endl;
+    cin >> kvadrat;
+    for (int i = 0; i < kvadrat; i++) {
+        for (int j = 0; j < i; j++) {
+            cout << "*" << " ";
+        }
+        cout << endl;
+    }*/
+
+    // Задание 4.
+    /*int a, counter = 1;
+    cout << endl << "Введите число не равное  нулю: " << endl;
+    cin >> a;
+    while (a / 10 != 0) {
+       
+        a = a / 10;
+        counter++;
+    }
+    
+    cout << endl << "Введенное число состоит из " << counter <<" разрядов" << endl;*/
+
+    // Задание 5
+    /*int nachal, konec;
+    cout << endl << "Введите границы диапазона: " << endl;
+    cin >> nachal >> konec;
+   
+    cout << endl;
+
+    int Arr[10];
+    for (int i = 0; i < 10; i++) {
+        cout << nachal + rand() % (konec - nachal + 1) << " ";
+    }*/
+
+    // Задание 6
+    // Задание 7
+    // Задание 8
+    // Задание 9
+    // Задание 10
+    // Задание 11
+    // Задание 12
+    // Задание 13
+   
+
+    // Задание 14
+    /*struct Auto {
+        string color;
+        string model;
+    };
+
+    Auto first;
+    first.color = "white";
+    first.model = "mercedes";
+
+    Auto second;
+    second.color = "green";
+    second.model = "audi";
+
+    Auto third;
+    third.color = "black";
+    third.model = "bmw";
+
+    Auto List[3];
+    
+    List[0] = first;
+    List[1] = second;
+    List[2] = third;
+
+    for (int i = 0; i < 3; i++) {
+        cout << List[i].color << " " << List[i].model << endl;
+    }*/
+    
+
+
+
+
+
+
+
+
+
+
+    
+}
